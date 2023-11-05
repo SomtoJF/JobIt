0155a365793491e740d18129cff9b6cb9236b844
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-05 03:28:29 +0100

    Import top half home components

--- app/index.js
+++ app/index.js
@@ -1,10 +1,39 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, Text, ScrollView, View } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, SIZES, icons, images } from "../constants";
+import {
+	Nearbyjobs,
+	Popularjobs,
+	ScreenHeaderBtn,
+	Welcome,
+} from "../components";
 
 const Home = () => {
+	const router = useRouter();
 	return (
-		<View>
-			<Text>Hello World</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+			<Stack.Screen
+				options={{
+					headerStyle: { backgroundColor: COLORS.lightWhite },
+					headerShadowVisible: false,
+					headerLeft: () => (
+						<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+					),
+					headerRight: () => (
+						<ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+					),
+					headerTitle: "",
+				}}
+			/>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<View style={{ padding: SIZES.medium }}>
+					<Welcome />
+					<Popularjobs />
+					<Nearbyjobs />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 export default Home;
