831bd7c8a79494ddfeb2f5bcb3ae82a072e5e386
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-05 12:57:39 +0100

    Create structure for popular jobs section

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -9,12 +9,38 @@ import {
 } from "react-native";
 import styles from "./popularjobs.style";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { COLORS, SIZES } from "../../../constants";
 
 export default function Popularjobs() {
+	const isLoading = false;
+	const error = false;
 	const router = useRouter();
+
 	return (
-		<View>
-			<Text>Popular Jobs</Text>
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={{ ...styles.headerTitle, fontWeight: "500" }}>
+					Popular Jobs
+				</Text>
+				<TouchableOpacity onPress={() => {}}>
+					<Text style={styles.headerBtn}>Show all</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator size={"large"} color={COLORS.primary} />
+				) : error ? (
+					"Something went wrong"
+				) : (
+					<FlatList
+						data={[1, 2, 3, 4]}
+						renderItem={({ item }) => <PopularJobCard item={item} />}
+						keyExtractor={(item) => item?.job_id}
+						contentContainerStyle={{ columnGap: SIZES.medium }}
+						horizontal
+					/>
+				)}
+			</View>
 		</View>
 	);
 }
