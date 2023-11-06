c3f53c8e2c0fc3865b8562b712610c3c6fa1f169
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-06 22:56:47 +0100

    Hydrate ui with nearby jobs information

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,9 +1,43 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { View, Text } from "react-native";
-export default function Nearbyjobs() {
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import styles from "./nearbyjobs.style";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import { COLORS } from "../../../constants";
+import useFetch from "../../../hooks/useFetch";
+
+export default function NearbyJobs() {
+	const router = useRouter();
+	const { data, isLoading, error } = useFetch("search", {
+		query: "react developer",
+		num_pages: 1,
+	});
+
 	return (
-		<View>
-			<Text>Nearbyjobs</Text>
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={{ ...styles.headerTitle, fontWeight: "500" }}>
+					Nearby Jobs
+				</Text>
+				<TouchableOpacity onPress={() => {}}>
+					<Text style={styles.headerBtn}>Show all</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator size={"large"} color={COLORS.primary} />
+				) : error ? (
+					"Something went wrong"
+				) : (
+					data?.map((job) => (
+						<NearbyJobCard
+							job={job}
+							key={job?.job_id}
+							handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+						/>
+					))
+				)}
+			</View>
 		</View>
 	);
 }
