2b3def8168049704687fd7e56b587473e32299dd
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-08 20:46:35 +0100

    BugFix: Use fetch hook undefined in dynamic route

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -42,3 +42,44 @@ export default function useFetch(endpoint, query) {
 
 	return { data, isLoading, error, refetch };
 }
+
+const useFetchJobDetails = (query) => {
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const options = {
+		method: "GET",
+		url: "https://jsearch.p.rapidapi.com/job-details",
+		headers: {
+			"X-RapidAPI-Key": APIKEY,
+			"X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+		},
+		params: { ...query },
+	};
+
+	const fetchData = async () => {
+		setIsLoading(true);
+		try {
+			const response = await axios.request(options);
+			setData(response.data.data);
+		} catch (error) {
+			setError(error);
+			alert("An error occured");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const refetch = () => {
+		fetchData();
+	};
+
+	return { data, isLoading, error, refetch };
+};
+
+export { useFetchJobDetails };
