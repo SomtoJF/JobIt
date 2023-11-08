efb9bd6479830912dc11aa5163153a67564fa727
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-08 21:14:37 +0100

    Populate job-details route with company info

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,9 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
-export default function Company() {
+import { View, Text, Image } from "react-native";
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
+
+export default function Company({
+	companyLogo,
+	jobTitle,
+	companyName,
+	location,
+}) {
 	return (
-		<View>
-			<Text>Company</Text>
+		<View style={styles.container}>
+			<View style={styles.logoBox}></View>
+			<Image
+				source={{
+					uri: checkImageURL(companyLogo)
+						? companyLogo
+						: icons.fallbackCompanyLogo,
+				}}
+				style={styles.logoImage}
+			/>
+			<View style={styles.jobTitleBox}>
+				<Text style={styles.jobTitle}>{jobTitle}</Text>
+			</View>
+			<View style={styles.companyInfoBox}>
+				<Text style={styles.companyName}>{companyName} / </Text>
+				<View style={styles.locationBox}>
+					<Image
+						source={icons.location}
+						resizeMode="contain"
+						style={styles.locationImage}
+					/>
+				</View>
+			</View>
 		</View>
 	);
 }
