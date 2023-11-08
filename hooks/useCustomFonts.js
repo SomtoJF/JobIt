250dcfce665a181d896dd77a7182b3a034e3c2cf
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-08 22:01:30 +0100

    Try using new technique to load fonts

--- hooks/useCustomFonts.js
+++ hooks/useCustomFonts.js
@@ -1,9 +1,20 @@
-import { useFonts } from "expo-font";
+import {
+	useFonts,
+	DMSans_400Regular,
+	DMSans_400Regular_Italic,
+	DMSans_500Medium,
+	DMSans_500Medium_Italic,
+	DMSans_700Bold,
+	DMSans_700Bold_Italic,
+} from "@expo-google-fonts/dm-sans";
 
 export default function useCustomFont() {
 	return useFonts({
-		DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-		DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-		DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+		DMSans_400Regular,
+		DMSans_400Regular_Italic,
+		DMSans_500Medium,
+		DMSans_500Medium_Italic,
+		DMSans_700Bold,
+		DMSans_700Bold_Italic,
 	});
 }
