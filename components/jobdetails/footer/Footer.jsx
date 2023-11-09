b02c4e464f4ec2fd607c1123da0fa69f4edfd00d
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-09 22:01:48 +0100

    Display job-details footer

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Linking, Pressable, Text, View } from "react-native";
+import styles from "./footer.style";
+import { icons } from "../../../constants";
 
-export default function Footer() {
+export default function Footer({ url }) {
 	return (
-		<View>
-			<Text>Footer</Text>
+		<View style={styles.container}>
+			<Pressable style={styles.likeBtn}>
+				<Image
+					style={styles.likeBtnImage}
+					source={icons.heartOutline}
+					resizeMode="contain"
+				/>
+			</Pressable>
+			<Pressable
+				style={styles.applyBtn}
+				onPress={() => {
+					Linking.openURL(url);
+				}}
+			>
+				<Text style={styles.applyBtnText}>Apply for job</Text>
+			</Pressable>
 		</View>
 	);
 }
